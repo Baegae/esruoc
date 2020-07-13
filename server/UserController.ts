cfe525c345c61c5942c05c0d668a2b8976115802
sjuuun <osjuny11@gmail.com>
2020-07-13 23:10:07 +0900

    style: eslinter 설정

--- server/UserController.ts
+++ server/UserController.ts
@@ -1,31 +1,31 @@
-import {Controller, Param, Body, Get, Post, Put, Delete} from "routing-controllers";
+import {Controller, Param, Body, Get, Post, Put, Delete} from 'routing-controllers';
 
 @Controller()
 export class UserController {
 
-    @Get("/users")
-    getAll() {
-        return "This action returns all users";
+    @Get('/users')
+  getAll() {
+    return 'This action returns all users';
+  }
+
+    @Get('/users/:id')
+    getOne(@Param('id') id: number) {
+      return 'This action returns user #' + id;
     }
 
-    @Get("/users/:id")
-    getOne(@Param("id") id: number) {
-        return "This action returns user #" + id;
-    }
-
-    @Post("/users")
+    @Post('/users')
     post(@Body() user: any) {
-        return "Saving user...";
+      return 'Saving user...';
     }
 
-    @Put("/users/:id")
-    put(@Param("id") id: number, @Body() user: any) {
-        return "Updating a user...";
+    @Put('/users/:id')
+    put(@Param('id') id: number, @Body() user: any) {
+      return 'Updating a user...';
     }
 
-    @Delete("/users/:id")
-    remove(@Param("id") id: number) {
-        return "Removing user...";
+    @Delete('/users/:id')
+    remove(@Param('id') id: number) {
+      return 'Removing user...';
     }
 
 }
