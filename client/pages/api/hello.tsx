491dab03068e77cf5e15e32100aba515cde46586
Yejun Kim <no1expman@gmail.com>
2020-07-13 22:55:41 +0900

    refactor: 필요없는 api 삭제

--- client/pages/api/hello.tsx
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { NextApiResponse, NextApiRequest } from 'next';
-
-export default (_: NextApiRequest, res: NextApiResponse): void => {
-  res.statusCode = 200;
-  res.json({ name: 'John Doe' });
-};
