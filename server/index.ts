3b674d180249edf7c4ce083f22afc47e43470067
Seungjun Oh <47017729+sjuuun@users.noreply.github.com>
2020-07-13 23:20:08 +0900

    feat(server): routing-controller와 bolierplate 추가 (#2)
    * feat: build directory gitignore에 추가
    
    * feat: tsconfig 추가
    
    * feat: package 추가
    
    * feat: Boilerplate 추가
    
    * chore: merge lock file
    
    * style: eslinter 설정
    
    * style: linter 적용
    
    * chore: tsconfig 수정

---
+++ server/index.ts
@@ -0,0 +1,11 @@
+import 'reflect-metadata'; // this shim is required
+import {createExpressServer} from 'routing-controllers';
+import {UserController} from './UserController';
+
+// creates express app, registers all controller routes and returns you express app instance
+const app = createExpressServer({
+  controllers: [UserController] // we specify controllers we want to use
+});
+
+console.log('Server Started!!');
+app.listen(3000);
