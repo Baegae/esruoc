72dbacc14a4c31b6d76fe59adad7dd5568335239
Dora Lee <dsa28s@naver.com>
2020-07-15 22:51:20 +0900

    style: 린트 적용 / 수정사항 반영

--- shared/src/entity/User.ts
+++ shared/src/entity/User.ts
@@ -1,18 +1,20 @@
-import {InProgressLecture} from "./InProgressLecture";
+import InProgressLecture from './InProgressLecture';
 
-export interface User {
+interface User {
     // 이름
-    name: string
+    name: string;
     // 이메일
-    email: string
+    email: string;
     // 구글 액세스 토큰
-    googleAccessToken: string
+    googleAccessToken: string;
     // 선생님 여부 (true : 선생님 / false : 학생)
-    isTeacher: boolean
+    isTeacher: boolean;
     // 수강 중인 강의 목록 (학생인 경우)
-    inProgressCourses: [InProgressLecture]
+    inProgressCourses: [InProgressLecture];
     // 만든 강의 아이디 (선생님일 경우)
-    uploadedCourses: [string]
+    uploadedCourses: [string];
     // 가입 시각
-    createdAt: Date
+    createdAt: Date;
 }
+
+export default User;
