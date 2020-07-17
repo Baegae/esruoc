63b4f379616d216e5008cf5f4412ec8106dc01ee
Dora Lee <dsa28s@naver.com>
2020-07-18 02:05:10 +0900

    fix: module-alias 모듈이 작동되지 않아 빌드 / 실행이 되지 않는 문제 수정

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import 'module-alias/register';
+import 'reflect-metadata'; // this shim is required
+
+import {createExpressServer} from 'routing-controllers';
+import {connect as connectMongoDB} from '@database/Database';
+
+const PORT = 8000;
+
+const app = createExpressServer({
+  controllers: []
+});
+
+connectMongoDB();
+
+console.log(`[INFO] Server started on port=${PORT}`);
+app.listen(8000);
