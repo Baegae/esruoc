cad4dedea4a61fa5369bf8c528c3c434dbacc7c7
Yejun Kim <no1expman@gmail.com>
2020-07-18 10:48:32 +0900

    chore: style 정리

--- client/src/components/VideoEdit/index.tsx
+++ client/src/components/VideoEdit/index.tsx
@@ -9,124 +9,121 @@ import Editor from '../Editor';
 import {RecoilRoot, atom, useRecoilState, selector, useRecoilValue} from 'recoil/dist';
 import {OutputData} from '@editorjs/editorjs';
 
-const VideoEdit: React.FC /**
- *
- */
-  = () => {
+const VideoEdit: React.FC = () => {
 
-    const [videoEdit, setVideoEdit] = useRecoilState(videoEditState);
-    useEffect(() => {
-      console.log('ATOM', videoEdit);
-    }, [videoEdit]);
+  const [videoEdit, setVideoEdit] = useRecoilState(videoEditState);
+  useEffect(() => {
+    console.log('ATOM', videoEdit);
+  }, [videoEdit]);
 
-    const resetRecording = () => {
-      setVideoEdit((state) => ({
-        ...state,
-        changes: [],
-        viewingChangeIndex: -1,
-        recordingStartedAt: new Date().getTime(),
-        previewVideoObjectUrl: undefined,
-        previewCurrentTime: 0,
-      }));
-    };
+  const resetRecording = () => {
+    setVideoEdit((state) => ({
+      ...state,
+      changes: [],
+      viewingChangeIndex: -1,
+      recordingStartedAt: new Date().getTime(),
+      previewVideoObjectUrl: undefined,
+      previewCurrentTime: 0,
+    }));
+  };
 
-    const addTextChange = (textData: OutputData) => {
-      setVideoEdit((videoEdit) => ({
-        ...videoEdit,
-        // originalEditorData,
-        changes: [...videoEdit.changes, { data:textData, videoTimestamp: (new Date().getTime() - videoEdit.recordingStartedAt) / 1000 }]
-      }));
-    };
+  const addTextChange = (textData: OutputData) => {
+    setVideoEdit((videoEdit) => ({
+      ...videoEdit,
+      // originalEditorData,
+      changes: [...videoEdit.changes, { data:textData, videoTimestamp: (new Date().getTime() - videoEdit.recordingStartedAt) / 1000 }]
+    }));
+  };
 
-    const setIsRecording = (isRecording: boolean) => {
-      setVideoEdit((videoEdit) => ({...videoEdit, isRecording}));
-    };
+  const setIsRecording = (isRecording: boolean) => {
+    setVideoEdit((videoEdit) => ({...videoEdit, isRecording}));
+  };
 
-    const setPreviewUrl = (url: string) => {
-      setVideoEdit((videoEdit) => ({...videoEdit, previewVideoObjectUrl: url}));
-    };
+  const setPreviewUrl = (url: string) => {
+    setVideoEdit((videoEdit) => ({...videoEdit, previewVideoObjectUrl: url}));
+  };
 
-    const moveChangeIndex = (delta: number) => {
-      setVideoEdit((state) => {
-        const nextIndex = state.viewingChangeIndex + delta;
-        if (!(nextIndex >= -1 && nextIndex < state.changes.length)) {
-          return state;
-        }
-        return ({...state, viewingChangeIndex: state.viewingChangeIndex + delta});
-      });
-    };
+  const moveChangeIndex = (delta: number) => {
+    setVideoEdit((state) => {
+      const nextIndex = state.viewingChangeIndex + delta;
+      if (!(nextIndex >= -1 && nextIndex < state.changes.length)) {
+        return state;
+      }
+      return ({...state, viewingChangeIndex: state.viewingChangeIndex + delta});
+    });
+  };
 
-    const setPreviewCurrentTime = (currentTime: number) => {
-      setVideoEdit((state) => ({...state, previewCurrentTime: currentTime}));
-    };
+  const setPreviewCurrentTime = (currentTime: number) => {
+    setVideoEdit((state) => ({...state, previewCurrentTime: currentTime}));
+  };
 
-    const videoRefCallback = getCameraMirrorRefCallback();
+  const videoRefCallback = getCameraMirrorRefCallback();
 
-    useEffect(() => {
-      navigator.mediaDevices.getUserMedia({audio: true, video: true}).then((stream) => {
-        recorder = new RecordRTC(stream, {type: 'video', video: {width: 1920, height: 1080}});
-      });
-    }, []);
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({audio: true, video: true}).then((stream) => {
+      recorder = new RecordRTC(stream, {type: 'video', video: {width: 1920, height: 1080}});
+    });
+  }, []);
 
-    const handleStartClick = () => {
+  const handleStartClick = () => {
     // TODO: 예전 텍스트로 리셋하기
-      if (videoEdit.isRecording) {
+    if (videoEdit.isRecording) {
+      return;
+    }
+    setIsRecording(true);
+    resetRecording();
+    recorder.startRecording();
+  };
+
+  const handleStopClick = () => {
+    recorder.stopRecording(() => {
+      setPreviewUrl(recorder.toURL());
+      setIsRecording(false);
+      // uploadVideoToServer(recorder.getBlob()).then(() => {
+      //   alert('upload ok!');
+      // });
+    });
+  };
+
+  const handleTextDataChange = useCallback<(data: OutputData) => void>(
+    (textData) => {
+      if (!videoEdit.isRecording) {
         return;
       }
-      setIsRecording(true);
-      resetRecording();
-      recorder.startRecording();
-    };
+      addTextChange(textData);
+    }, [addTextChange, videoEdit.isRecording]);
 
-    const handleStopClick = () => {
-      recorder.stopRecording(() => {
-        setPreviewUrl(recorder.toURL());
-        setIsRecording(false);
-        // uploadVideoToServer(recorder.getBlob()).then(() => {
-        //   alert('upload ok!');
-        // });
-      });
-    };
-
-    const handleTextDataChange = useCallback<(data: OutputData) => void>(
-      (textData) => {
-        if (!videoEdit.isRecording) {
-          return;
-        }
-        addTextChange(textData);
-      }, [addTextChange, videoEdit.isRecording]);
-
-    return <Scaffold>
-      <EditorContainer>
-        <Editor
-          data={useRecoilValue(editorTextDataState)}
-          onChange={handleTextDataChange}
+  return <Scaffold>
+    <EditorContainer>
+      <Editor
+        data={useRecoilValue(editorTextDataState)}
+        onChange={handleTextDataChange}
+      />
+    </EditorContainer>
+    <VideoContainer>
+      <CameraVideo
+        ref={videoRefCallback}
+        autoPlay
+        controls={false}
+        muted
+      />
+      {!videoEdit.isRecording
+        ? <RecordButton onClick={handleStartClick}>start recording</RecordButton>
+        : <RecordButton onClick={handleStopClick}>stop recording</RecordButton>}
+      {videoEdit.previewVideoObjectUrl && <div>
+        <h2>Record Preview</h2>
+        <PreviewVideo
+          onTimeUpdate={(event) => {
+            setPreviewCurrentTime(event.currentTarget.currentTime);
+          }}
+          src={videoEdit.previewVideoObjectUrl}
+          controls
+          width="250"
         />
-      </EditorContainer>
-      <VideoContainer>
-        <CameraVideo
-          ref={videoRefCallback}
-          autoPlay
-          controls={false}
-          muted
-        />
-        {!videoEdit.isRecording
-          ? <RecordButton onClick={handleStartClick}>start recording</RecordButton>
-          : <RecordButton onClick={handleStopClick}>stop recording</RecordButton>}
-        {videoEdit.previewVideoObjectUrl && <div>
-          <h2>Record Preview</h2>
-          <PreviewVideo
-            onTimeUpdate={(event) => {
-              setPreviewCurrentTime(event.currentTarget.currentTime);
-            }}
-            src={videoEdit.previewVideoObjectUrl}
-            controls
-            width="250"
-          />
-        </div>}
-      </VideoContainer>
-    </Scaffold>;
-  };
+      </div>}
+    </VideoContainer>
+  </Scaffold>;
+};
 
 export default () => <RecoilRoot><VideoEdit/></RecoilRoot>;
 
