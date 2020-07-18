7ea3b2231e30dd6a0c17abca2d805082d64f89d6
sjuuun <osjuny11@gmail.com>
2020-07-18 22:39:06 +0900

    feat: 헤더에서 Authorization 파싱하는 함수 추가

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,27 @@
 import 'reflect-metadata';
 
-import {createExpressServer} from 'routing-controllers';
+import {Action, createExpressServer} from 'routing-controllers';
 import swaggerUI from 'swagger-ui-express';
 import {connect as connectMongoDB} from '@database/Database';
 import {initFirebase} from '@database/Firebase';
 import {LectureController} from '@controller/LectureController';
 import {UserController} from '@controller/UserController';
+import {decodeJWT} from '@business/AuthService';
+import UserRepository from '@repository/UserRepository';
 
 import swaggerConfig from '../swagger.config';
 
 const PORT = 8000;
 
 const swaggerSpec = swaggerConfig;
+const userRepository = new UserRepository();
 const app = createExpressServer({
-  controllers: [LectureController, UserController]
+  controllers: [LectureController, UserController],
+  currentUserChecker: async (action: Action, value?: any) => {
+    const token = action.request.headers['authorization'];
+    const uid = decodeJWT(token);
+    return userRepository.findUserByUid(uid);
+  }
 });
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
