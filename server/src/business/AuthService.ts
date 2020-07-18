95f6b8b0bc9bc08e95981c106eed54c37fe11456
sjuuun <osjuny11@gmail.com>
2020-07-18 21:07:08 +0900

    feat: JWT 관련 함수 추가

--- server/src/business/AuthService.ts
+++ server/src/business/AuthService.ts
@@ -1,10 +1,26 @@
 import process from 'process';
 import jwt from 'jsonwebtoken';
+import User from '@shared/entity/User';
 
-export function encodeJWT(userId: string): string {
+export function encodeJWT(user: User): string {
   const authKey = process.env.AUTH_KEY;
   return jwt.sign(
-    {'userId': userId},
-    authKey
+    {
+      'uid': user.uid,
+      'name': user.name,
+      'email': user.email,
+      'profileImageUrl': user.profileImageUrl
+    },
+    authKey!,
+    {expiresIn: '7d'}
   );
+}
+
+export function verifyJWT(token: string): void {
+  const authKey = process.env.AUTH_KEY;
+  jwt.verify(token, authKey!);
+}
+
+export function decodeJWT(token: string): null | { [key: string]: any } | string {
+  return jwt.decode(token);
 }
