60942982ffe256a1cbfbca1d45d71b18ad24ca3d
sjuuun <osjuny11@gmail.com>
2020-07-18 21:18:32 +0900

    docs: 로그인 swagger 문서 추가

--- server/src/controller/UserController.ts
+++ server/src/controller/UserController.ts
@@ -7,6 +7,53 @@ export class UserController {
 
   private userService: UserService = new UserService()
 
+  /**
+   * @swagger
+   * tags:
+   *   name: User
+   *   description: 유저 객체
+   * definitions:
+   *   User:
+   *     type: object
+   *     properties:
+   *       uid:
+   *         type: string
+   *         description: 유저 Google ID
+   *       name:
+   *         type: string
+   *         description: 유저 이름
+   *       email:
+   *         type: string
+   *         description: 유저 email
+   *       profileImageUrl:
+   *         type: string
+   *         description: 유저 프로필 사진 Url
+   *       createdAt:
+   *         type: Date
+   *         description: 유저가 생성된 시각
+   */
+
+  /**
+   * @swagger
+   * /user/login:
+   *   post:
+   *     summary: 유저 로그인
+   *     consumes:
+   *       - application/json
+   *     parameters:
+   *      - in: body
+   *        name: googleAccesstoken
+   *        description: Firebase AccessToken
+   *        type: string
+   *
+   *     tags: [User]
+   *     responses:
+   *       200:
+   *         description: Authorization token
+   *         schema:
+   *           type: string
+   */
+
   @Post('/login')
   login(@Body() loginInput: LoginInput) {
     return this.userService.login(loginInput);
