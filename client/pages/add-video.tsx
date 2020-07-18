587107eb02c3f2cb44c9aa0e0ec57d6d98eea0c2
kattergil <gil@beomjun.kr>
2020-07-19 03:42:51 +0900

    feat: AddVideo 페이지에 EditorToolBar추가

--- client/pages/add-video.tsx
+++ client/pages/add-video.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
+import EditorToolBar from '@src/components/editor-page/EditorToolBar';
 
 const VideoEditTest = dynamic(() => import('../src/components/VideoEdit'), {
   ssr: false,
@@ -7,6 +8,11 @@ const VideoEditTest = dynamic(() => import('../src/components/VideoEdit'), {
 
 export default () => (
   <div>
+    <EditorToolBar
+      name="비디오 테스트"
+      lectureName="강의 이름"
+    />
+    <div style={{height: 120}} />
     <VideoEditTest />
   </div>
 );
