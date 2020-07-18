3476c1adb24fccd756127e8e22f6778f19bed92d
Dora Lee <dsa28s@naver.com>
2020-07-18 17:20:37 +0900

    feat(backend): 강의 추가 API 추가

--- server/src/business/LectureService.ts
+++ server/src/business/LectureService.ts
@@ -1,16 +1,28 @@
 import CreateLectureRequest from '@shared/request/CreateLectureRequest';
 import LectureRepository from '@repository/LectureRepository';
+import {getDownloadUrl, uploadFileToStorage} from '@database/Firebase';
+import CreateLectureResponse from '@shared/response/CreateLectureResponse';
 
 class CreateLectureService {
-  lectureRepository = new LectureRepository();
+    lectureRepository = new LectureRepository();
 
-  async createLecture(lectureMainImage: any, payload: CreateLectureRequest) {
-    await this.lectureRepository.saveLecture({
-      title: payload.title,
-      description: payload.description,
-      uploaderId: 'dsa28s'
-    });
-  }
+    async createLecture(lectureMainImage: any, payload: CreateLectureRequest): Promise<CreateLectureResponse> {
+      const savedLecture = await this.lectureRepository.saveLecture({
+        title: payload.title,
+        description: payload.description,
+        uploaderId: 'dsa28s'
+      });
+
+      await uploadFileToStorage(`lecture/${savedLecture._id}`, 'MainImage', lectureMainImage);
+      const mainImageUrl = await getDownloadUrl(`lecture/${savedLecture._id}/MainImage`);
+
+      return {
+        id: savedLecture._id.toString(),
+        title: savedLecture.title,
+        description: savedLecture.description,
+        mainImageUrl
+      };
+    }
 }
 
 export default CreateLectureService;
--- server/src/controller/LectureController.ts
+++ server/src/controller/LectureController.ts
@@ -1,19 +1,69 @@
 import {BodyParam, JsonController, Post, UploadedFile} from 'routing-controllers';
 import LectureService from '@business/LectureService';
-import SuccessResponse from '@controller/SuccessResponse';
+import CreateLectureResponse from '@shared/response/CreateLectureResponse';
 
 @JsonController('/lecture')
 export class LectureController {
     lectureService = new LectureService();
 
+    /**
+     * @swagger
+     * tags:
+     *   name: Lecture
+     *   description: 강의 객체
+     * definitions:
+     *   Lecture:
+     *     type: object
+     *     properties:
+     *       id:
+     *         type: string
+     *         description: 강의 ID
+     *       title:
+     *         type: string
+     *         description: 강의 제목
+     *       description:
+     *         type: string
+     *         description: 강의 설명
+     *       mainImageUrl:
+     *         type: string
+     *         description: 강의 이미지 다운로드 URL
+     */
+
+    /**
+     * @swagger
+     * /lecture:
+     *   post:
+     *     summary: 강의 생성
+     *     consumes:
+     *       - application/form-data
+     *     parameters:
+     *      - in: formData
+     *        name: title
+     *        type: string
+     *        description: 강의 제목
+     *      - in: formData
+     *        name: description
+     *        type: string
+     *        description: 강의 설명
+     *      - in: formData
+     *        name: mainImage
+     *        type: binary
+     *        description: 메인 이미지 파일
+     *
+     *     tags: [Lecture]
+     *     responses:
+     *       200:
+     *         description: 생성된 강의 정보
+     *         schema:
+     *           type: object
+     *           $ref: '#/definitions/Lecture'
+     */
     @Post('')
     async createLecture(@BodyParam('title') title: string,
                   @BodyParam('description') description: string,
-                  @UploadedFile('lectureMainImage') lectureMainImage?: any) {
-      await this.lectureService.createLecture(lectureMainImage, {
+                  @UploadedFile('lectureMainImage') lectureMainImage?: any): Promise<CreateLectureResponse> {
+      return await this.lectureService.createLecture(lectureMainImage, {
         title: title, description: description
       });
-
-      return SuccessResponse;
     }
 }
