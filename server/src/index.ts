52f605a98810fccb5a0dbde4bf6938f38eb92d5b
Dora Lee <dsa28s@naver.com>
2020-07-18 17:20:07 +0900

    feat(backend): swagger 문서 연동

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,21 @@
 import 'reflect-metadata';
 
 import {createExpressServer} from 'routing-controllers';
+import swaggerUI from 'swagger-ui-express';
 import {connect as connectMongoDB} from '@database/Database';
 import {LectureController} from '@controller/LectureController';
 import {initFirebase} from '@database/Firebase';
 
+import swaggerConfig from '../swagger.config';
+
 const PORT = 8000;
 
+const swaggerSpec = swaggerConfig;
 const app = createExpressServer({
   controllers: [LectureController],
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 connectMongoDB();
 initFirebase();
