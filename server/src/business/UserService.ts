d32684a0d17711a75d34c6dfedeecb030020a3cb
sjuuun <osjuny11@gmail.com>
2020-07-18 21:06:32 +0900

    feat: User Schema에 프로필 이미지 Url 추가

--- server/src/business/UserService.ts
+++ server/src/business/UserService.ts
@@ -3,6 +3,8 @@ import * as admin from 'firebase-admin';
 import UserRepository from '@repository/UserRepository';
 import UserRecord = admin.auth.UserRecord;
 import {UnauthorizedError} from 'routing-controllers';
+import {encodeJWT} from '@business/AuthService';
+import User from '../../../shared/src/entity/User';
 
 export class UserService {
   private userRepository = new UserRepository();
@@ -14,23 +16,24 @@ export class UserService {
       const uid = decodedToken.uid;
       const userRecord = await admin.auth().getUser(uid);
 
-      if (await this.userRepository.findUserByUid(userRecord.uid) == null) {
-        this.saveUser(userRecord);
+      let user = await this.userRepository.findUserByUid(userRecord.uid) as User;
+      if (user == null) {
+        user = await this.saveUser(userRecord);
       }
 
-      return uid;
+      return encodeJWT(user);
     } catch (e) {
       throw new UnauthorizedError('인증 정보가 잘못되었습니다.');
     }
   }
 
-  private saveUser(userRecord: UserRecord) {
-    this.userRepository.saveUser({
+  private async saveUser(userRecord: UserRecord): Promise<User> {
+    return await this.userRepository.saveUser({
       uid: userRecord.uid,
       name: userRecord.displayName!,
       email: userRecord.email!,
+      profileImageUrl: userRecord.photoURL!,
       createdAt: new Date()
     });
   }
-
 }
--- shared/src/entity/User.ts
+++ shared/src/entity/User.ts
@@ -7,6 +7,8 @@ interface User {
     name: string;
     // 이메일
     email: string;
+    // 프로필 이미지 url
+    profileImageUrl: string;
     // 수강 중인 강의 목록 (학생인 경우)
     inProgressCourses?: [InProgressLecture];
     // 만든 강의 아이디 (선생님일 경우)
