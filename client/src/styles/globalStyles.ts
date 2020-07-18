14ed929d90d41cc5ef621fbc6f450ca146842532
kattergil <gil@beomjun.kr>
2020-07-18 12:25:46 +0900

    style: GlobalStyles 추가

---
+++ client/src/styles/globalStyles.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+import normalizeCss from './normalize';
+
+const GlobalStyles = createGlobalStyle`
+  ${normalizeCss};
+
+  html {
+    font-family: -apple-system, BlinkMacSystemFont, "Apple SD Gothic Neo", "Malgun Gothic", "맑은 고딕", helvetica, sans-serif;
+    letter-spacing: -0.025em;
+
+    &.spoqa-han-sans-loaded {
+      font-family: 'Spoqa Han Sans', sans-serif;
+    }
+  }
+`;
+
+export default GlobalStyles;
