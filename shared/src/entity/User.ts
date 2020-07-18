1fdec8388989a93cbf36ebe8398da3c1c1e3f08a
sjuuun <osjuny11@gmail.com>
2020-07-18 17:43:50 +0900

    fix: User interface 수정

--- shared/src/entity/User.ts
+++ shared/src/entity/User.ts
@@ -1,18 +1,16 @@
 import InProgressLecture from './InProgressLecture';
 
 interface User {
+    // ID
+    uid: string;
     // 이름
     name: string;
     // 이메일
     email: string;
-    // 구글 액세스 토큰
-    googleAccessToken: string;
-    // 선생님 여부 (true : 선생님 / false : 학생)
-    isTeacher: boolean;
     // 수강 중인 강의 목록 (학생인 경우)
-    inProgressCourses: [InProgressLecture];
+    inProgressCourses?: [InProgressLecture];
     // 만든 강의 아이디 (선생님일 경우)
-    uploadedCourses: [string];
+    uploadedCourses?: [string];
     // 가입 시각
     createdAt: Date;
 }
