3e294340d2719c907a2440ff48c26a175de11505
Dora Lee <dsa28s@naver.com>
2020-07-18 10:51:32 +0900

    feat(backend): Hot Reload 를 지원하고, Firebase 앱을 초기화할 수 있도록 Credential 파일 추가 / 클래스 추가

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,18 @@
-import 'module-alias/register';
 import 'reflect-metadata';
 
 import {createExpressServer} from 'routing-controllers';
 import {connect as connectMongoDB} from '@database/Database';
+import {LectureController} from '@controller/LectureController';
+import {initFirebase} from '@database/Firebase';
 
 const PORT = 8000;
 
 const app = createExpressServer({
-  controllers: []
+  controllers: [LectureController],
 });
 
 connectMongoDB();
+initFirebase();
 
 console.log(`[INFO] Server started on port=${PORT}`);
 app.listen(8000);
