7fc8f9341091ebe4f4cd1341934b408ba2d3db95
sjuuun <osjuny11@gmail.com>
2020-07-19 13:30:59 +0900

    feat(server): User에 description 추가

--- shared/src/entity/User.ts
+++ shared/src/entity/User.ts
@@ -7,6 +7,8 @@ interface User {
     name: string;
     // 이메일
     email: string;
+    // 유저 설명
+    description: string;
     // 프로필 이미지 url
     profileImageUrl: string;
     // 수강 중인 강의 목록 (학생인 경우)
