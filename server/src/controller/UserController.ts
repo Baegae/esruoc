9611451a97f8bbde3322fd026733bf5b55ba1f7e
sanghun.lee <dsa28s@naver.com>
2020-07-19 14:20:05 +0900

    fix(server): 일부 누락된 필드 변경

--- server/src/controller/UserController.ts
+++ server/src/controller/UserController.ts
@@ -2,36 +2,35 @@ import {Body, JsonController, Post} from 'routing-controllers';
 import LoginInput from '@shared/request/LoginInput';
 import {UserService} from '@business/UserService';
 
+/**
+ * @swagger
+ * tags:
+ *   name: User
+ *   description: 유저와 관련 있는 API 들
+ * definitions:
+ *   User:
+ *     type: object
+ *     properties:
+ *       uid:
+ *         type: string
+ *         description: 유저 Google ID
+ *       name:
+ *         type: string
+ *         description: 유저 이름
+ *       email:
+ *         type: string
+ *         description: 유저 email
+ *       profileImageUrl:
+ *         type: string
+ *         description: 유저 프로필 사진 Url
+ *       createdAt:
+ *         type: Date
+ *         description: 유저가 생성된 시각
+ */
 @JsonController('/user')
 export class UserController {
 
   private userService: UserService = new UserService()
-
-  /**
-   * @swagger
-   * tags:
-   *   name: User
-   *   description: 유저 객체
-   * definitions:
-   *   User:
-   *     type: object
-   *     properties:
-   *       uid:
-   *         type: string
-   *         description: 유저 Google ID
-   *       name:
-   *         type: string
-   *         description: 유저 이름
-   *       email:
-   *         type: string
-   *         description: 유저 email
-   *       profileImageUrl:
-   *         type: string
-   *         description: 유저 프로필 사진 Url
-   *       createdAt:
-   *         type: Date
-   *         description: 유저가 생성된 시각
-   */
 
   /**
    * @swagger
