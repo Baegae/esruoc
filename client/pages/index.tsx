4af74a69a165e2b49f0bd72610a5171a1268c98a
sanghun.lee <dsa28s@naver.com>
2020-07-19 15:31:44 +0900

    feat(client): 로그인 페이지 추가

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,36 +1,77 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Container} from 'react-grid-system';
+import {Col, Container, Row} from 'react-grid-system';
 
+import Illustration from '@src/assets/illustration.svg';
+import Logo from '@src/assets/logo.svg';
+import CTAButton from '@src/components/common/CTAButton';
 
-const TestHeader = styled.h1`
-  color: ${({theme}) => theme.colors.text.default};
+const HomeContainer = styled(Container)`
+  position: absolute !important;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
-const TestCaption = styled.p`
-  color: ${({theme}) => theme.colors.text.caption};
+const Center = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
-const TestPrimaryColor = styled.div`
-  background: ${({theme}) => theme.colors.primary};
-  width: 64px;
-  height: 64px;
+const IllustImage = styled(Illustration)`
+  width: 200%;
+  position: relative;
+  right: calc(100% - 50px);
 `;
 
-const TestAccentColor = styled.div`
-  background: ${({theme}) => theme.colors.accent};
-  width: 64px;
-  height: 64px;
+const LogoImage = styled(Logo)`
+  width: 60%;
+`;
+
+const AlignLeftContainer = styled.div`
+  margin-left: 75px;
+`;
+
+const Title = styled.p`
+  font-size: 1.5rem;
+  color: rgba(51, 58, 61, 0.55);
+  letter-spacing: -0.8px;
+`;
+
+const LoginButton = styled(CTAButton)`
+  margin-top: 82px;
+  flex: 0 0 auto;
 `;
 
 const Home: React.FC = () => {
   return (
-    <Container>
-      <TestHeader>안녕하세여여어어어ㅓ</TestHeader>
-      <TestCaption>너무 반갑다는 뜻;;</TestCaption>
-      <TestPrimaryColor />
-      <TestAccentColor />
-    </Container>
+    <HomeContainer>
+      <Row>
+        <Col md={6}>
+          <Center>
+            <IllustImage />
+          </Center>
+        </Col>
+        <Col md={6}>
+          <Center>
+            <LogoImage />
+            <AlignLeftContainer>
+              <Title>새로운 방식으로 바꿀 시간입니다.</Title>
+              <Title>나만의 코스를 만들어보세요.</Title>
+              <div>
+                <LoginButton>로그인하기</LoginButton>
+              </div>
+            </AlignLeftContainer>
+          </Center>
+        </Col>
+      </Row>
+    </HomeContainer>
   );
 };
 
