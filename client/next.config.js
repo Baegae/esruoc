8faf8ecc8b4f2c857a1b0a4b0021376e90ce2642
sanghun.lee <dsa28s@naver.com>
2020-07-19 14:20:26 +0900

    feat(client): next.js 설정에 환경변수 추가

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,15 @@
 const path = require('path');
+const process = require('process');
 
 module.exports = {
+  env: {
+    GOOGLE_FIREBASE_STORAGE_BUCKET: process.env.GOOGLE_FIREBASE_STORAGE_BUCKET,
+    GOOGLE_FIREBASE_API_KEY: process.env.GOOGLE_FIREBASE_API_KEY,
+    GOOGLE_FIREBASE_AUTH_DOMAIN: process.env.GOOGLE_FIREBASE_AUTH_DOMAIN,
+    GOOGLE_FIREBASE_PROJECT_ID: process.env.GOOGLE_FIREBASE_PROJECT_ID,
+    GOOGLE_FIREBASE_APP_ID: process.env.GOOGLE_FIREBASE_APP_ID,
+    GOOGLE_FIREBASE_MEASUREMENT_ID: process.env.GOOGLE_FIREBASE_MEASUREMENT_ID,
+  },
   webpack(config, _) {
     config.resolve = {
       alias: {
