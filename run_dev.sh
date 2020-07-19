aae2735835c4e79543778d02b98be5e54d4ea5fe
sanghun.lee <dsa28s@naver.com>
2020-07-19 14:17:51 +0900

    chore: 도커 이미지를 실행하면 프론트 프로젝트도 같이 실행되도록 변경

--- run_dev.sh
+++ run_dev.sh
@@ -4,4 +4,5 @@ mongod --bind_ip_all --fork --dbpath /srv/mongodb --logpath /var/log/mongodb.log
 
 cd /lectus
 
+nohup yarn workspace client run dev  > /dev/null 2>&1 &
 yarn workspace server run start
